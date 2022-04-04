3e5a7d4b1e899597d2afe3a1c04b8c0fe1c20618
adityaarcot <arcot.aditya@utexas.edu>
2022-04-04 09:52:30 -0500

    Set up RecyclerView

--- app/src/main/java/com/example/grocerylist/GroceryListActivity.kt
+++ app/src/main/java/com/example/grocerylist/GroceryListActivity.kt
@@ -1,31 +1,23 @@
 package com.example.grocerylist
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.example.grocerylist.adapter.ItemAdapter
+import com.example.grocerylist.data.Datasource
 
 class GroceryListActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_grocery_list)
+        setContentView(R.layout.activity_grocery_list_new)
 
-        val item_1_button = findViewById<Button>(R.id.item_1)
-        val item_2_button = findViewById<Button>(R.id.item_2)
+        // Initialize data
+        val myDataset = Datasource().loadListItems()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
 
-        item_1_button.setOnClickListener {
-            openItemInfoActivity("Item 1")
-        }
-        item_2_button.setOnClickListener {
-            openItemInfoActivity("Item 2")
-        }
     }
 
-    fun openItemInfoActivity(itemName:String) {
-        val intent = Intent(this, ItemInfoActivity::class.java).apply {
-            putExtra("itemName", itemName)
-        }
-        startActivity(intent)
-    }
 
 }
