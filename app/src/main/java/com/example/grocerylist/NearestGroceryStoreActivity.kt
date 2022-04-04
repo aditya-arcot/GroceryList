b502b4b7881b25199c224cfacf953ea237709f9f
ashlilii <ashleylee@utexas.edu>
2022-04-04 10:56:14 -0500

    ashley - gps and map

--- app/src/main/java/com/example/grocerylist/NearestGroceryStoreActivity.kt
+++ app/src/main/java/com/example/grocerylist/NearestGroceryStoreActivity.kt
@@ -1,11 +1,68 @@
 package com.example.grocerylist
 
+import android.Manifest
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import androidx.core.content.ContextCompat
+import com.google.android.gms.maps.model.MarkerOptions
+import android.content.pm.PackageManager
 
-class NearestGroceryStoreActivity : AppCompatActivity() {
+class NearestGroceryStoreActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nearest_grocery_store)
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as? SupportMapFragment
+        mapFragment?.getMapAsync(this)
+
+        if (ContextCompat.checkSelfPermission(this@NearestGroceryStoreActivity,
+                Manifest.permission.ACCESS_COARSE_LOCATION) !==
+            PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this@NearestGroceryStoreActivity,
+                    Manifest.permission.ACCESS_COARSE_LOCATION)) {
+                ActivityCompat.requestPermissions(this@NearestGroceryStoreActivity,
+                    arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), 1)
+            } else {
+                ActivityCompat.requestPermissions(this@NearestGroceryStoreActivity,
+                    arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), 1)
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>,
+                                            grantResults: IntArray) {
+        when (requestCode) {
+            1 -> {
+                if (grantResults.isNotEmpty() && grantResults[0] ==
+                    PackageManager.PERMISSION_GRANTED) {
+                    if ((ContextCompat.checkSelfPermission(this@NearestGroceryStoreActivity,
+                            Manifest.permission.ACCESS_COARSE_LOCATION) ===
+                                PackageManager.PERMISSION_GRANTED)) {
+                        Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
+                    }
+                } else {
+                    Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
+                }
+                return
+            }
+        }
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        val sydney = LatLng(-33.852, 151.211)
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(sydney)
+                .title("Marker in Sydney")
+        )
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 }
